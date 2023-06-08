87ffe8b359b815ad2d85e6d48efe8c3b5c822fb7
jmd <jmd@flauz-laptop.localdomain>
2023-06-08 11:06:30 +0200

    add validators alert role

--- src/routes/alerts.routes.js
+++ src/routes/alerts.routes.js
@@ -2,8 +2,11 @@ const router = require("express").Router();
 
 const { getAllAlerts, getAlert, addAlert, deleteAlert, editAlert } = require("../controller/alertController.js");
 
+const { validateAlert } = require("../validator/alertValidator");
+
+
 router.get("/", getAllAlerts);
-router.post("/", addAlert);
+router.post("/", validateAlert, addAlert);
 router.get("/:id", getAlert);
 router.delete("/:id", deleteAlert);
 router.put("/:id", editAlert);
--- src/routes/roles.routes.js
+++ src/routes/roles.routes.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 
 const { getAllRoles, getRole, addRole, deleteRole, editRole } = require("../controller/roleController.js");
 
+const { validateRoleName } = require("../validator/roleValidator");
+
 
 router.get("/", getAllRoles);
-router.post("/", addRole);
+router.post("/", validateRoleName, addRole);
 router.get("/:id", getRole);
 router.delete("/:id", deleteRole);
 router.put("/:id", editRole);
