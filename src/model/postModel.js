22454a15260a07a4ddb53de8fec2ecf4b08afa5b
etienne chamarier <etienne.chamarier@gmail.com>
2023-06-23 12:08:55 +0200

    user_post_vote

--- src/model/postModel.js
+++ src/model/postModel.js
@@ -12,18 +12,6 @@ const findAll = () => {
         })
 } 
 
-const findOnePost = (id) => {
-    return db
-        .execute("select * from post where id = ?", [id])
-        .then(([data]) => {
-            return data;
-        })
-        .catch((err) =>{
-            console.error("Error ", err)
-            return err;
-        })
-} 
-
 const createPost = (post) => {
     const { title, description, createdDate, status, profit, risk, avatar, user_id, category, location, impact, deadlineDate, makeDecisionDate, conflitDate, vote } = post;
     console.log('post --->', post)
@@ -69,4 +57,91 @@ const modifyPost = (post, id) => {
         })
 } 
 
-module.exports = { findAll, findOnePost, createPost, removePost, modifyPost };
+const findOnePost = (id) => {
+    return db
+        .execute("select * from post where id = ?", [id])
+        .then(([data]) => {
+            return data;
+        })
+        .catch((err) =>{
+            console.error("Error ", err)
+            return err;
+        })
+} 
+
+const createVote = (addVote) => {
+    const { user_id, post_id, vote } = addVote;
+
+    return db
+        .execute("insert into user_post_vote (user_id, post_id, vote) values (?, ?, ?)",
+        [user_id, post_id, vote])
+        .then (([data]) => {
+            return { id: data.insertId, ...addVote };
+        })
+        .catch((err) =>{
+            console.error("error", err)
+            return err;
+        })
+}
+
+const findVoteFromPost = (id) => {
+    return db
+        .execute("select u.firstname, u.lastname, upv.vote from user as u inner join user_post_vote as upv on u.id = upv.user_id where post_id = ?", [id])
+        .then(([data]) => {
+            return data;
+        })
+        .catch((err) =>{
+            console.error("Error ", err)
+            return err;
+        })
+    }
+
+const findVoteFromUser = (id) => {
+        return db
+            .execute("select p.title, upv.vote from post as p inner join user_post_vote as upv on p.id = upv.post_id where upv.user_id = ?", [id])
+            .then(([data]) => {
+                return data;
+            })
+            .catch((err) =>{
+                console.error("Error ", err)
+                return err;
+            })
+        }
+
+const findCountVote = (vote) => {
+    return db
+    .execute("select count(vote) from user_post_vote where vote = ?", [vote])
+    .then(([data]) => {
+        return data;
+    })
+    .catch((err) =>{
+        console.error("Error ", err)
+        return err;
+    })
+}
+
+const findCountAllVoteFromPost = (vote) => {
+    return db
+    .execute("select post_id, count(vote) from user_post_vote where vote = ? group by post_id;", [vote])
+    .then(([data]) => {
+        return data;
+    })
+    .catch((err) =>{
+        console.error("Error ", err)
+        return err;
+    })
+}
+
+const findCountPositiveAndNegativeVote = (postId) => {
+    return db
+    .execute("SELECT COUNT(*) AS total_votes, SUM(vote = 1) AS positive_vote, SUM(vote = 0) AS negative_vote FROM user_post_vote WHERE post_id = ?;", [postId])
+    .then(([data]) => {
+        return data;
+    })
+    .catch((err) =>{
+        console.error("Error ", err)
+        return err;
+    })
+}
+
+module.exports = { findAll, findOnePost, createPost, removePost, modifyPost, createVote, findVoteFromPost, findVoteFromUser, findCountVote, findCountAllVoteFromPost,findCountPositiveAndNegativeVote};
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const { getAllPosts, getPost, addPost, deletePost, editPost } = require("../controller/postController.js");
+const { getAllPosts, getPost, addPost, deletePost, editPost, getVoteFromUser,getVoteFromPost, addVote, countVote, countPositiveAndNegativeVote,countAllVoteFromPost } = require("../controller/postController.js");
 
 const { validatePost } = require("../validator/postValidator.js");
 
@@ -9,5 +9,12 @@ router.post("/", validatePost, addPost);
 router.get("/:id", getPost);
 router.delete("/:id", deletePost);
 router.put("/:id", editPost);
+router.get("/votes/fromuser/:id", getVoteFromUser);
+router.get("/votes/frompost/:id", getVoteFromPost);
+router.post("/votes", addVote);
+router.get("/votes/countvote/:count", countVote);
+router.get("/votes/allvotepost/:id", countPositiveAndNegativeVote);
+router.get("/votes/votepost/:count", countAllVoteFromPost )
+
 
 module.exports = router;
