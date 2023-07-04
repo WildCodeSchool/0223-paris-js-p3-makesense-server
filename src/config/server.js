cf433123754a130e50a668e95156604fc98e134b
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-07-04 17:31:31 +0200

    ajout script

--- src/config/server.js
+++ src/config/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const cookieParser = require("cookie-parser");
 const app = express();
 const router = require('../routes/index.routes');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
-app.use(helmet())
+app.use(helmet());
+app.use(cookieParser());
 
 app.use('/api', router);
 
---
+++ src/validator/authValidator.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const authorization = (req, res, next) => {
+    const token = req.cookies.access_token;
+    
+    if (!token) {
+      return res.sendStatus(401);
+    }
+    try {
+      const data = jwt.verify(token, process.env.JWT_AUTH_SECRET);
+      req.userId = data.id;
+      req.userRole = data.role;
+      return next();
+    } catch {
+      return res.sendStatus(401);
+    }
+}; 
+
+const isAdmin = (req, res, next) => {
+    if (req.userRole === "ROLE_ADMIN") {
+      return next();
+    }
+    return res.sendStatus(403);
+};
+
+module.exports = { authorization, isAdmin };
